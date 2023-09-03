885299c455211d66f77444d8cd8c503e6c7e0365
github.4o <githubmail.4o@ya.ru>
2023-09-03 11:20:14 +0700

    some wild changes

--- lottery.py
+++ lottery.py
@@ -1,13 +1,17 @@
 import random
+import traceback
+import queue
 from discord.ext import commands, tasks
+from discord.utils import get
 from datetime import datetime
 from pd import pd
 
 
-def setup(bot):
+async def setup(bot):
     l = lottery(bot)
-    bot.add_cog(l)
+    await bot.add_cog(l)
     l.handout.start()
+    l.delete_msgs.start()
 
 
 guid = 871762312208474133
@@ -18,7 +22,6 @@ class lottery(commands.Cog):
         print('lottery module loaded')
         self.bot = bot
         self.pd = pd('prizes.json')
-        print(self.pd)
         for i in ['participants', 'prizes']:
             if i not in self.pd:
                 print('resetting {} in ctor'.format(i))
@@ -30,14 +33,41 @@ class lottery(commands.Cog):
             print('resetting last date')
             self.pd['last_date'] = ''
         self.pd.sync()
+        hugs = open('hugs.txt').read()
+        self.hugs = [x for x in hugs.split('\n\n') if x != '']
+        self.q = queue.Queue()
+        self.msg_to_delete = None
 
     @commands.command()
     async def lottery(self, ctx, *args):
-        print('old', self.pd)
         if args[0] == 'set' and args[1] == 'channel':
             a = self.set_channel(ctx)
+        elif args[0] == 'hugs':
+            a = '```\n' + str(self._hugs()) + '```'
         elif args[0] == 'print':
-            a = str(self.pd)
+            a = str({k: v for k,v in self.pd.items() if k != 'bribe'})
+        elif args[0] == 'bribe':
+            if ctx.channel.id != 875942917489983508:
+                await ctx.send('please, use #bot-spam channel for bribes')
+                return
+            nl = 50
+            if 'bribe' not in self.pd:
+                self.pd['bribe'] = random.randrange(nl)
+                self.pd.sync()
+            if len(args) == 1:
+                a = 'gimme that number. like `.lottery bribe 12` or whatever number you have'
+            else:
+                try:
+                    num = int(args[1])
+                    if num == self.pd['bribe']:
+                        self.pd['bribe'] = random.randrange(nl)
+                        self.pd['participants'].append(ctx.author.id)
+                        self.pd.sync()
+                        a = 'woohoo, just what i wanted'
+                    else:
+                        a = f'nop, dont like that number. try nymbers 0-{nl-1}'
+                except:
+                    a = 'thats not a number'
         elif args[0] == 'handout':
             if ctx.author.id == self.bot.id4o:
                 try:
@@ -51,11 +81,19 @@ class lottery(commands.Cog):
             else:
                 a = 'snif snif. nop, you dont smell like 4o'
         elif args[0] == 'add' and args[1] == 'prize':
+            await ctx.send('youre about to add prize(s) to the cookie jar')
             a = ''
+            dry = False
+            if args[-1] == '--dry':
+                args = args[:-1]
+                dry = True
             for i in args[2:]:
-                self.pd['prizes'].append([ctx.author.id, i])
-                self.pd.sync()
-                a += 'user {} added {} to prize pool'.format(self._print_user(ctx.author.id), i)
+                if not dry:
+                    self.pd['prizes'].append([ctx.author.id, i])
+                    self.pd.sync()
+                a += 'user {} added {} to prize pool\n'.format(self._print_user(ctx.author.id), i)
+                if dry:
+                    a += 'no prizes were added due to --dry option\n'
         elif args[0] == 'participants':
             a = str(self.pd['participants'])
         elif args[0] == 'prizes':
@@ -63,7 +101,6 @@ class lottery(commands.Cog):
         else:
             a = 'invalid command: {}'.format(*args)
         await self.bot.send(ctx, a)
-        print('new', self.pd)
 
     def set_channel(self, ctx):
         self.pd['channel'] = ctx.channel.id
@@ -72,39 +109,70 @@ class lottery(commands.Cog):
         
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author.bot == False:
+        if message.author.bot:
+            return
+
+        member = message.author
+
+        role = get(member.guild.roles, name='corp member')
+        
+        if role not in member.roles:
+            return
+
+        if message.guild:
             if message.guild.id == guid:
                 if self._add_participant(message.author.id):
                     msg = "user {} takes part in this week's lottery ({} participants so far)".format(self._print_user(message.author.id), len(self.pd['participants']))
-                    await self.bot.send(message.channel.id, msg)
+                    self.q.put(await message.channel.send(msg))
 
     def cog_unload(self):
+        print('killing lottery cog')
         self.handout.cancel()
+
+    @tasks.loop(seconds = 5)
+    async def delete_msgs(self):
+        try:
+            await self.msg_to_delete.delete()
+            self.msg_to_delete = None
+        except Exception as e:
+            pass
+        try:
+            msg = self.q.get(block = False)
+            self.msg_to_delete = msg
+        except:
+            pass
 
     @tasks.loop(hours = 4)
     async def handout(self, force = False):
-        print('old: ', self.pd)
         try:
             await self._handout(force)
         except Exception as e:
             print(e)
-        print('new: ', self.pd)
 
     async def _handout(self, force):
         mf = '%Y%m%d'
         if force == False:
             if self.pd['last_date'] == datetime.now().strftime(mf):
-                print('no time for handouts', self.pd['last_date'], datetime.now().strftime(mf))
+                print('last date check failed')
                 return
-        print('pulling winner from list')
+        if len(self.pd['prizes']) == 0:
+            await self.bot.send(self.pd['channel'], 'cookie jar empty. say`.lottery add prize <prize>` to add prize')
+#            await self.bot.send(self.pd['channel'], f'no prizes <@{uid4o}>. adding default set')
+#            self.pd['prizes'].append([uid4o, 't8ticket'])
+#            self.pd['prizes'].append([uid4o, 't8ticket'])
+#            self.pd['prizes'].append([uid4o, 'hugs'])
+#            self.pd['prizes'].append([uid4o, 'hugs'])
+#            self.pd['prizes'].append([uid4o, 'hugs'])
+#            self.pd['prizes'].append([uid4o, 'hugs'])
+#            self.pd['prizes'].append([uid4o, 'hugs'])
         wn = len(self.pd['participants'])
         if wn == 0:
             print('nobody participate. sadly')
             return
         rnd_w = random.randrange(wn)
         w = self.pd['participants'][rnd_w]
-        print('winner:', w)
-        print('pulling prize from list')
+        if w in [743638088080687224, 276102320708648960]:
+            return
         pl = [x for x in self.pd['prizes'] if x[0] != w]
         pn = len(pl)
         if pn == 0:
@@ -117,6 +185,8 @@ class lottery(commands.Cog):
         msg += '{} please give the promissed cookie immediately\n'.format(self._print_user(p[0]))
         msg += 'participants list is cleared. please send any msg to take part in next round\n'
         msg += 'rnd info: prize {}/{} participant {}/{}\n'.format(rnd_p, pn, rnd_w, wn)
+        if p[1].startswith('hugs'):
+            msg += f'```\n{self._hugs()}```'
         await self.bot.send(self.pd['channel'], msg)
         print('cleaning up')
         print('upating date')
@@ -139,3 +209,10 @@ class lottery(commands.Cog):
 
     def _print_user(self, uid):
         return '<@' + str(uid) + '>'
+
+    def _hugs(self):
+        n = len(self.hugs)
+        rnd_p = random.randrange(n)
+        return self.hugs[rnd_p]
+
+
